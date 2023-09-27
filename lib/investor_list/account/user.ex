30da4e75591d02609b83ac06d04adbaed9b38179
Brandon Richey <richeyb@gmail.com>
2023-09-26 23:16:50 -0400

    Times up! Added tests, added some minor functionality to reduce file clobbering

--- lib/investor_list/account/user.ex
+++ lib/investor_list/account/user.ex
@@ -52,7 +52,7 @@ defmodule InvestorList.Account.User do
   defp validate_password(changeset, opts) do
     changeset
     |> validate_required([:password])
-    |> validate_length(:password, min: 12, max: 72)
+    |> validate_length(:password, min: 4, max: 72) # TODO: Only making this 4 for demo purposes, I wouldn't do this in a real project
     # Examples of additional password validation:
     # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
